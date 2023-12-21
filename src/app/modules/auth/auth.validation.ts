906b2448f2e2cc535ae026f3d3a6a1a309961674
FaizulOsman <faizulosmanrabby@gmail.com>
2023-12-21 13:59:24 +0600

    feat: user and admin crud functionality incremented

---
+++ src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,38 @@
+import { z } from 'zod';
+
+const createUserZodSchema = z.object({
+  body: z.object({
+    email: z.string(),
+    password: z.string(),
+    role: z.enum(['user', 'admin']),
+    name: z.string(),
+    phone: z.string().optional(),
+    imageUrl: z.string().optional(),
+    address: z.string().optional(),
+  }),
+});
+
+const loginUserZodSchema = z.object({
+  body: z.object({
+    email: z.string({
+      required_error: 'email is required',
+    }),
+    password: z.string({
+      required_error: 'password is required',
+    }),
+  }),
+});
+
+const refreshTokenZodSchema = z.object({
+  cookies: z.object({
+    refreshToken: z.string({
+      required_error: 'refreshToken is required',
+    }),
+  }),
+});
+
+export const authValidation = {
+  createUserZodSchema,
+  loginUserZodSchema,
+  refreshTokenZodSchema,
+};
---
+++ src/app/modules/user/user.constants.ts
@@ -0,0 +1,18 @@
+export const role = ['user', 'admin', 'super_admin'];
+
+export const UserSearchableFields = [
+  'role',
+  'phoneNumber',
+  'name',
+  'email',
+  'address',
+];
+
+export const UserFilterableFields = [
+  'searchTerm',
+  'role',
+  'phoneNumber',
+  'name',
+  'email',
+  'address',
+];
